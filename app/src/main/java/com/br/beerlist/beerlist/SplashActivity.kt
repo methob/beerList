76610d3f4b7dce1f5fbd970161ac5b1f5123bfcf
jonathan <jonathannascimento70@gmail.com>
2018-01-31 00:09:56 -0300

    :tada: - commit first project structure.

---
+++ app/src/main/java/com/br/beerlist/beerlist/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.br.beerlist.beerlist
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Observable.just(true).
+                delay(2, TimeUnit.SECONDS).
+                subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread()).
+                map {
+                    callListBeersActivity()
+                }.subscribe()
+    }
+
+    fun callListBeersActivity() {
+        val intent = Intent(this@SplashActivity, ListBeerActivity::class.java)
+        startActivity(intent)
+    }
+}
+
